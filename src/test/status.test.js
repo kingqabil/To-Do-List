c9822e793b66c11c6cf5ea38074af6c29a65d77b
kingqabil <kingqabil@live.com>
2021-12-05 15:15:10 +0300

    Some corrections

--- src/test/status.test.js
+++
@@ -1,9 +0,0 @@
-import status from '../status.js';
-
-jest.mock('../status');
-
-describe('Status change function', () => {
-  test('Check list item when status changes', () => {
-    expect(status()).toBe(true);
-  });
-});
