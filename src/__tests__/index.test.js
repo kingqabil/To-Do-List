516bf5b203e0b8a970fc11f4f51bb8162cb60585
kingqabil <kingqabil@live.com>
2021-12-15 16:25:39 +0300

    Update index.test.js

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -17,22 +17,5 @@ jest.mock('../addItem');
 jest.mock('../clearAll');
 
 describe('Add function', () => {
-  test('Add one new item to the list', () => {
-    expect(add().length).toBe(2);
-  });
-  test('Remove all completed items from list', () => {
-    expect(clearAll().length).toBe(1);
-  });
-  test('Edit a list item', () => {
-    expect(editItem()).toBe('some state...');
-  });
-  test('Update the index of list when one item is deleted', () => {
-    expect(indexUpdate()).toEqual([1, 2]);
-  });
-  test('Remove one new item from the list', () => {
-    expect(removeItem().length).toBe(0);
-  });
-  test('Check list item when status changes', () => {
-    expect(status()).toBe(true);
-  });
+  
 });
